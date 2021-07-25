08275b563ac34a84a4d23b35f6bd555391bdac73
arcinston <arushkurundodi@gmail.com>
2021-07-25 15:52:24 +0530

    some light modifications

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Waddup biyactch!</Text>
+      <Text>Waddup bitch!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
