5a5c66c4d1b97ba62f8ba3a62ccc1cb2c7ed8117
arcinston <arushkurundodi@gmail.com>
2021-07-25 19:21:45 +0530

    app.js has beta homepage

--- App.js
+++ App.js
@@ -1,22 +1,82 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableNativeFeedback,
+  Image,
+  StatusBar,
+  SafeAreaView,
+  Platform,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("image tapped ");
   return (
-    <View style={styles.container}>
-      <Text>Waddup bitch!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[
+        styles.container,
+        { justifyContent: "flex-end", alignContent: "center" },
+      ]}
+    >
+      <Image
+        source={require("./assets/sun.png")}
+        style={{
+          height: 100,
+          width: 100,
+          top: 200,
+          alignSelf: "center",
+          position: "absolute",
+        }}
+      ></Image>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          color: "#fff",
+          top: 350,
+          alignSelf: "center",
+          position: "absolute",
+        }}
+      >
+        Say what you feel , when you feel
+      </Text>
+      <View
+        style={{
+          backgroundColor: "#fc0341",
+          height: 75,
+          justifyContent: "flex-end",
+        }}
+      >
+        <Text style={styles.text}>Sign In</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#03fca5",
+          height: 75,
+          justifyContent: "flex-end",
+        }}
+      >
+        <Text style={styles.text}>Sign Up</Text>
+      </View>
+    </SafeAreaView>
   );
-  ``;
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "black",
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
+    flexDirection: "column",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 40,
     justifyContent: "center",
+    alignSelf: "center",
+    paddingBottom: 20,
   },
 });
